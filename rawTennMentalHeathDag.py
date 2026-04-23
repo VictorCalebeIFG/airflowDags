1c12e13f490fddd8d975487c952d3554221e8474
Victor Calebe Cavalcante de Macedo <victorcalebeacd@gmail.com>
2026-04-23 18:22:29 -0300

    ffix

--- rawTennMentalHeathDag.py
+++ rawTennMentalHeathDag.py
@@ -18,7 +18,21 @@ with DAG(
         skip_leading_rows=1,
         autodetect=False,
         write_disposition="WRITE_TRUNCATE",
-        gcp_conn_id="google_cloud_default"
+        gcp_conn_id="google_cloud_default",
+        schema_fields = [
+            {"name": "age", "type": "INTEGER"},
+            {"name": "gender", "type": "STRING"},
+            {"name": "daily_social_media_hours", "type": "FLOAT"},
+            {"name": "platform_usage", "type": "STRING"},
+            {"name": "sleep_hours", "type": "FLOAT"},
+            {"name": "screen_time_before_sleep", "type": "FLOAT"},
+            {"name": "academic_performance", "type": "FLOAT"},
+            {"name": "physical_activity", "type": "FLOAT"},
+            {"name": "social_interaction_level", "type": "STRING"},
+            {"name": "stress_level", "type": "INTEGER"},
+            {"name": "anxiety_level", "type": "INTEGER"},
+            {"name": "addiction_level", "type": "INTEGER"},
+            {"name": "depression_label", "type": "INTEGER"},]
     )
 
     gcstobigqueryoperator
