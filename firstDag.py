0adc1ebe70413266def9ca06c0b2e7d847d7c3f2
Victor Calebe Cavalcante de Macedo <victorcalebeacd@gmail.com>
2026-04-23 13:15:53 -0300

    firstcommit

---
+++ firstDag.py
@@ -0,0 +1,21 @@
+from datetime import datetime
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+
+def firstTask():
+    print('hello world !')
+
+with DAG (
+    dag_id = 'firstDag',
+    start_date = datetime(2026,4,23),
+    schedule_interval = '@once'
+    catchup = False,
+    tags = ['estudo']
+) as dag:
+
+    firstTask = PythonOperator(
+        task_id = 'firstTask',
+        python_callable = firstTask
+    )
+
+    firstTask
